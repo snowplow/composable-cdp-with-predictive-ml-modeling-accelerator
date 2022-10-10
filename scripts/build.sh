55ad6a98401e3e294095b8cc9e15f0abe057974a
georgewoodhead <50772749+georgewoodhead@users.noreply.github.com>
2022-10-10 14:54:00 +0100

    fix build script

--- scripts/build.sh
+++ scripts/build.sh
@@ -7,14 +7,14 @@ cp config.toml build/config.toml
 cp -R content build/
 
 echo "Importing themes..."
+git submodule update --init --recursive
 mkdir build/themes
 cp -R accelerator-web-ui-template/themes/hugo-theme-learn build/themes/
 cp -R accelerator-web-ui-template/layouts build/
 cp -R accelerator-web-ui-template/static build/
-cd build
-git submodule update --init --recursive
 
 echo "Creating Hugo site..."
+cd build
 if [ $# -eq 2 ]
 then
 hugo --config baseconfig.toml,config.toml --gc --minify -d ../public$1 -b $2
